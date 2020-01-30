7f235c38737e53c535e60e27c7d0390ad27add87
Student <example@example.com>
2020-01-30 15:43:36 -0800

    Initial commit

--- Models/ApiHelper.cs
+++ Models/ApiHelper.cs
@@ -7,7 +7,7 @@ namespace MVC.Models
   {
     public static async Task<string> ApiCall()
     {
-      RestClient client = new RestClient("https://localhost:5003/");
+      RestClient client = new RestClient("http://localhost:5004/");
       RestRequest request = new RestRequest($"home.json", Method.GET);
       var response = await client.ExecuteTaskAsync(request);
       return response.Content;
